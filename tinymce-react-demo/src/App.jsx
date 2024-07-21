5d8f759790acb636d129ac6c7443ba87ef97ac84
Vishal Singh <126417458+vshl2706@users.noreply.github.com>
2024-07-21 11:28:20 +0530

    Blog App
    Blog App

---
+++ tinymce-react-demo/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
+export default function App() {
+  return (
+    <Editor
+      apiKey='9zl0jb8grnvi6qj50lj97wd3bwkkg8c7sk04x36h4qmhxexy'
+      init={{
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+      }}
+      initialValue="Welcome to TinyMCE!"
+    />
+  );
+}
